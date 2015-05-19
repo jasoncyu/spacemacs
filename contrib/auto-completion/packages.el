138e5b466a1e22731c900c6a298132fed2f11f69
Tu Do <tu.h.do@dektech.com.au>
2015-05-19 18:34:39 +0700

    [Fix #1512] Don't enable smartparens when expanding snippets
    Otherwise the snippets will be messed up when expanding with
    hippie-expand.

--- contrib/auto-completion/packages.el
+++ contrib/auto-completion/packages.el
@@ -224,6 +224,10 @@
                                                      shell-mode-hook)))
     :config
     (progn
+      (add-hook 'yas-before-expand-snippet-hook (lambda ()
+                                                  (smartparens-mode -1)))
+      (add-hook 'yas-after-exit-snippet-hook (lambda ()
+                                               (smartparens-mode 1)))
       (spacemacs|diminish yas-minor-mode " ⓨ" " y"))))
 
 (defun auto-completion/init-auto-yasnippet ()
